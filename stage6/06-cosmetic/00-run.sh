03800b5b510d07f78eccfe1bf62a9a1aac89f9ba
Mircea Caprioru <mircea.caprioru@analog.com>
2020-06-03 14:09:43 +0300

    stage6: 06-cosmetic: Remove screensaver
    This patch will disable the LXDE screensaver.
    
    Signed-off-by: Mircea Caprioru <mircea.caprioru@analog.com>

--- stage6/06-cosmetic/00-run.sh
+++ stage6/06-cosmetic/00-run.sh
@@ -6,4 +6,9 @@ cp -f files/launch.png "${ROOTFS_DIR}/usr/share/raspberrypi-artwork"
 on_chroot << EOF
 sed -i 's+^wallpaper=.*$+wallpaper=/usr/share/rpd-wallpaper/wallpaper.png+g' /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf
 sed -i 's+start-here+/usr/share/raspberrypi-artwork/launch.png+g' /etc/xdg/lxpanel/LXDE-pi/panels/panel
+
+#disable screensaver
+sed -i 's+@xscreensaver -no-splash+#@xscreensaver -no-splash+g' /etc/xdg/lxsession/LXDE-pi/autostart
+echo "@xset s off" >> /etc/xdg/lxsession/LXDE-pi/autostart
+echo "@xset -dpms" >> /etc/xdg/lxsession/LXDE-pi/autostart
 EOF
