6ebcc119375f1b68f168d19339e27fec099ef057
Mircea Caprioru <mircea.caprioru@analog.com>
2021-01-19 16:09:43 +0200

    extra-boot: Write preloader for Altera platforms
    This patch will write a default preloader for Altera devices.
    
    Signed-off-by: Mircea Caprioru <mircea.caprioru@analog.com>

--- stage8/02-extra-boot/00-run.sh
+++ stage8/02-extra-boot/00-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 if [ -n ${EXTRA_BOOT} ]; then
-	wget -r -nH --cut-dirs=4 -np -R "index.html*" "-l${EXTRA_BOOT_DIR_DEPTH}" "${EXTRA_BOOT}" -P "${STAGE_WORK_DIR}/rootfs/boot"
+	wget -r -nH --cut-dirs=5 -np -R "index.html*" "-l${EXTRA_BOOT_DIR_DEPTH}" "${EXTRA_BOOT}" -P "${STAGE_WORK_DIR}/rootfs/boot"
 fi
