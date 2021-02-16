cd6179719d9f86cff161650cf3c72f7de6d8d912
Mircea Caprioru <mircea.caprioru@analog.com>
2021-02-16 10:51:04 +0200

    stage4: Enable root autologin on serial
    This patch will enable root user to autologin on serial terminal.
    
    Signed-off-by: Mircea Caprioru <mircea.caprioru@analog.com>

--- stage4/01-console-autologin/00-run.sh
+++ stage4/01-console-autologin/00-run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
+install -m 644 files/serial-getty@.service "${ROOTFS_DIR}/lib/systemd/system/"
+
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_behaviour B4
+
+	systemctl enable serial-getty@ttyPS0.service
+	systemctl enable serial-getty@ttyS0.service
+	sed -i '1s/^/auth sufficient pam_listfile.so item=tty sense=allow file=\/etc\/securetty onerr=fail apply=root\n/' "/etc/pam.d/login"
+
 EOF
