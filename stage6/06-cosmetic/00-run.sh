teoperisanu/adi-kuiper-gen

commit 3cd5e83626d5cb06ca1e2ba688a1a2947f1ff08d

stage6: Check if file exists
Only remove 99-fbturbo.conf if it exists.

Signed-off-by: Mircea Caprioru <mircea.caprioru@analog.com>

stage6/06-cosmetic/00-run.sh
@@ -12,7 +12,9 @@ sed -i 's+@xscreensaver -no-splash+#@xscreensaver -no-splash+g' /etc/xdg/lxsessi
 echo "@xset s off" >> /etc/xdg/lxsession/LXDE-pi/autostart
 echo "@xset -dpms" >> /etc/xdg/lxsession/LXDE-pi/autostart
 
-rm /usr/share/X11/xorg.conf.d/99-fbturbo.conf
+[ -f "/usr/share/X11/xorg.conf.d/99-fbturbo.conf" ] || {
+	rm /usr/share/X11/xorg.conf.d/99-fbturbo.conf
+}
 
 #mark raspberrypi-ui-mods to hold
 apt-mark hold raspberrypi-ui-mods

commit 86a46d03d6fb0854e1e6f4c270cedce127743bee

stage6:06-cosmetic:00-run.sh: fix file deletion
Commit 3cd5e836 introduced a bug regarding deletion of a file.
Replace file exists condition with just 'rm -f <file name>'.
Using '-f' option will suppress output if the file does not exists.

Signed-off-by: stefan.raus <stefan.raus@analog.com>

stage6/06-cosmetic/00-run.sh
@@ -12,9 +12,7 @@ sed -i 's+@xscreensaver -no-splash+#@xscreensaver -no-splash+g' /etc/xdg/lxsessi
 echo "@xset s off" >> /etc/xdg/lxsession/LXDE-pi/autostart
 echo "@xset -dpms" >> /etc/xdg/lxsession/LXDE-pi/autostart
 
-[ -f "/usr/share/X11/xorg.conf.d/99-fbturbo.conf" ] || {
-	rm /usr/share/X11/xorg.conf.d/99-fbturbo.conf
-}
+rm -f /usr/share/X11/xorg.conf.d/99-fbturbo.conf
 
 #mark raspberrypi-ui-mods to hold
 apt-mark hold raspberrypi-ui-mods