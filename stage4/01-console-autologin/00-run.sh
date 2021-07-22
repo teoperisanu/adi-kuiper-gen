918244cb3599669dc5015816b5ccdb650d9174ff
Sergiu Cuciurean <sergiu.cuciurean@analog.com>
2021-07-22 07:35:16 +0300

    stage4: Add ttyGS0/1
    Add gadget serial for ACE plugin configuration.
    
    Signed-off-by: Sergiu Cuciurean <sergiu.cuciurean@analog.com>

--- stage4/01-console-autologin/00-run.sh
+++ stage4/01-console-autologin/00-run.sh
@@ -7,8 +7,12 @@ on_chroot << EOF
 
 	systemctl enable serial-getty@ttyPS0.service
 	systemctl enable serial-getty@ttyS0.service
+	systemctl enable serial-getty@ttyGS0.service
+	systemctl enable serial-getty@ttyGS1.service
 	sed -i '1s/^/auth sufficient pam_listfile.so item=tty sense=allow file=\/etc\/securetty onerr=fail apply=root\n/' "/etc/pam.d/login"
 
 	echo "ttyPS0" >> /etc/securetty
+	echo "ttyGS0" >> /etc/securetty
+	echo "ttyGS1" >> /etc/securetty
 
 EOF
