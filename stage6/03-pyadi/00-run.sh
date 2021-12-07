b5702db0ebf52af521eeedd023a9282f291d20e3
Sergiu Cuciurean <sergiu.cuciurean@analog.com>
2021-12-07 14:30:27 +0200

    stage6: 03-pyadi: Add pyadi-dt
    Add pyadi-dt to the installed packages
    
    Signed-off-by: Sergiu Cuciurean <sergiu.cuciurean@analog.com>

--- stage6/03-pyadi/00-run.sh
+++ stage6/03-pyadi/00-run.sh
@@ -3,6 +3,7 @@
 on_chroot << EOF
 
 pip3 install pyadi-iio
+pip3 install git+https://github.com/analogdevicesinc/pyadi-dt.git
 echo "export PYTHONPATH=\"${PYTHONPATH}:/lib/python3.7/site-packages\"" >> /home/analog/.bashrc
 
 EOF
